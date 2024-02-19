14c9e7b72d093225413ddb5ee7ccf435be7a627c
Masanov Jumabek <masanov3167@gmail.com>
2024-02-19 18:16:54 +0500

    keyboards

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Telegraf } from "telegraf";
+import { Markup, Telegraf } from "telegraf";
 import VARIABLES from "./utils/variables";
 
 const bot = new Telegraf(VARIABLES.token);
@@ -16,13 +16,26 @@ bot.hears(/^\d+$/, ctx => ctx.reply("botga raqam yuborildi"));
 bot.on("voice", ctx =>{
     console.log(ctx.message);
     const adminId = ctx.from.id;
-    ctx.telegram.sendMessage(adminId, `${ctx.from.first_name} botga ovozli habar yubordi`)
+    ctx.telegram.sendMessage(adminId, `${ctx.from.first_name} botga ovozli habar yubordi`);
     ctx.reply("ovozli habar");
-})
+});
+
+bot.hears("button", ctx =>ctx.reply("siz button ni bosdingiz"));
+
+bot.action("button", ctx => ctx.reply("siz inline buttonni bosdingiz"));
+bot.action(/^btn_[a-zA-Z]+$/, (ctx: any) => {
+    const data = ctx.update.callback_query.data;
+    ctx.reply(`${data} bosildi`);
+});
+
+bot.command("removebtn", ctx => ctx.reply("btn tozalandi",Markup.removeKeyboard()))
 
 bot.on("text", ctx =>{
     const text = ctx.message.text;
-    ctx.reply(`siz yuborgan matn: ${text}`);
+    const keyboard = [];
+    text.split(" ").forEach((i) => keyboard.push([{text:i}]));
+    const test  = Markup.keyboard(keyboard).resize();
+    ctx.reply(`siz yuborgan matn: ${text}`, test);
 });
 
 bot.launch({dropPendingUpdates:true});
