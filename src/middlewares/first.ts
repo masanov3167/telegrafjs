4ad3806d218f7020307b2ea43cb209d28fe4266b
Masanov Jumabek <masanov3167@gmail.com>
2024-02-29 22:12:12 +0500

    subscribe channel

--- src/middlewares/first.ts
+++ src/middlewares/first.ts
@@ -1,8 +1,34 @@
 import { BotCtx } from "../types/context";
+import VARIABLES from "../utils/variables";
 
 
-const first = (ctx: BotCtx, next : () => Promise<any>) => {
-    // ctx.reply("Salom first middlewaredan");
+const first = async (ctx: BotCtx, next : () => Promise<any>) => {
+    // if(ctx.from.id == VARIABLES.adminId){
+    //     next();
+    // }
+    const channels = [];
+
+    for(let i of VARIABLES.channles){
+        const result = await ctx.telegram.getChatMember(i.id,ctx.from.id);
+        if(result.status !== "member"){
+            channels.push([{text:i.title, url: `t.me/${i.username}`}]);
+        }
+    }
+
+    console.log(channels);
+    
+
+    if(channels.length){
+        ctx.reply("kanalga a'zo bo'l", {
+            reply_markup:{
+                inline_keyboard: [...channels,[
+                    {text:"Tasdiqlash", callback_data:"check"}
+                ]]
+            }
+        });
+        return 
+    }
+
     next();
 }
 
--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,5 +1,13 @@
 const VARIABLES ={
-    token:"6384921517:AAGEyInEYbiBnfx1-b1ZNs_OloYYwlWqymY"
+    token:"6384921517:AAGEyInEYbiBnfx1-b1ZNs_OloYYwlWqymY",
+    adminId: 334245,
+    channles: [
+        {
+        id: -1001142342166,
+        title: "Masanov Jumabek | video darslar | blog",
+        username: "masanov_jumabek",
+    }
+    ]
 }
 
 export default VARIABLES
