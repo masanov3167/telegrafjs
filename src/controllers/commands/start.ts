5ee946d7ae2207c0ec8c0bc4a40adab1c5a730a2
Masanov Jumabek <masanov3167@gmail.com>
2024-03-28 14:56:42 +0500

    web tasbeh

--- src/controllers/commands/start.ts
+++ src/controllers/commands/start.ts
@@ -2,7 +2,13 @@ import { BotCtx } from "../../types/context";
 import myFn from "../../utils/functions";
 
 const fn = (ctx: BotCtx) =>{
-    ctx.reply("Assalomu alaykum!")
+    ctx.reply("Assalomu alaykum! \n\nTelegram Web App namuna", {
+        reply_markup:{
+            inline_keyboard:[
+                [{text:"web app", web_app:{url:"https://webtasbeh.netlify.app/"}}]
+            ]
+        }
+    })
 }
 
 const startFn = (ctx) => myFn(ctx, fn);
